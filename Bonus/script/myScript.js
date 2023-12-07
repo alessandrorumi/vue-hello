6962d4033e677391cf81ac52dbfe95e7ed7111e1
alessandrorumi <rumi.alessandro21@gmail.com>
2023-12-07 15:04:17 +0100

    Bonus: img logo Vue JS

---
+++ Bonus/script/myScript.js
@@ -0,0 +1,17 @@
+// Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+
+const { createApp } = Vue;
+
+createApp({
+
+  data() {
+
+    return {
+      messaggio: 'Hello World',
+      foto: 'https://cdn.sanity.io/images/8edntncj/production/7cb22a8a9885468d96662f6ece61ceb2bc95666d-500x300.png',
+      logoVue: 'logo Vue JS'
+    }
+
+  }
+
+}).mount('#app')
