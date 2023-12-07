f27b985a2487a3478f822a37513696074772eb56
alessandrorumi <rumi.alessandro21@gmail.com>
2023-12-07 15:24:46 +0100

    Bonus: Switch colore titolo

--- Bonus/script/myScript.js
+++ Bonus/script/myScript.js
@@ -5,13 +5,23 @@ const { createApp } = Vue;
 createApp({
 
   data() {
-
     return {
       messaggio: 'Hello World',
       foto: 'https://cdn.sanity.io/images/8edntncj/production/7cb22a8a9885468d96662f6ece61ceb2bc95666d-500x300.png',
-      logoVue: 'logo Vue JS'
+      logoVue: 'logo Vue JS',
+      coloreTitolo: ''
     }
+  },
 
+  methods: {
+
+    changeBackgroundColor: function () {
+      if (this.coloreTitolo === '') {
+        this.coloreTitolo = 'blue'
+      } else {
+        this.coloreTitolo = ''
+      }
+    }
   }
 
 }).mount('#app')
